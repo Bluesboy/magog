0a6e360d8fb7af4b0f54e515fbaa4fa0f24e8c6b
Ph0zzy <afayzullin@plussell.org>
2018-06-05 17:13:28 +0500

    first successfull migration

--- pygog.py
+++ pygog.py
@@ -7,14 +7,18 @@ import urllib.request
 
 # argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--clone-addr", type = str, help = "source repo url", required = True)
-parser.add_argument("--repo-name", type = str, help = "repository name", required = True)
+parser.add_argument("--cloneaddr", type = str, help = "source repo url", required = True)
+parser.add_argument("--reponame", type = str, help = "repository name", required = True)
 parser.add_argument("-m", "--mirror", action="store_true", help = "Repository will be a mirror. Default is false")
 parser.add_argument("-p", "--private", action="store_true", help = "Repository will be private. Default is false")
 parser.add_argument("--description", type = str, help = "Repository description")
 parser.add_argument("--fqdn", type = str, help = "FQDN of target repos server")
+parser.add_argument("-o", "--owner", type = str, help = "Target repo owner", required = True)
+args = parser.parse_args()
 
 token = os.environ['GOGSTOKEN']
+user = os.environ['SOURCEUSER']
+passwd = os.environ['SOURCEPASSWD']
 headers = {
         'Authorization': "token {}".format(token),
         'Content-Type': "application/json",
@@ -26,3 +30,14 @@ def get_owner_id(owner, req_headers, target_fqdn):
     request = urllib.request.Request("http://{0}/api/v1/users/{1}".format(target_fqdn, owner), headers = req_headers, method = 'GET')
     with urllib.request.urlopen(request) as f:
         return json.loads(f.read().decode("utf-8"))['id']
+
+# migrate function
+def migrate(req_headers, repo_name, clone_addr, user_name, user_passwd, uid, description, private, target_fqdn):
+    body = json.dumps({ "clone_addr" : clone_addr, "auth_username" : user_name, "auth_password": user_passwd, "uid" : uid, "description" : description, "private" : private, "repo_name" : repo_name }).encode()
+    request = urllib.request.Request("http://{0}/api/v1/repos/migrate".format(target_fqdn), data = body, headers = req_headers, method = 'POST')
+    with urllib.request.urlopen(request) as f:
+        return json.loads(f.read().decode("utf-8"))
+
+uid = get_owner_id(args.owner, headers, args.fqdn)
+result = migrate(headers, args.reponame, args.cloneaddr, user, passwd, uid, args.description, args.private, args.fqdn)
+print(json.dumps(result, indent = 4, ensure_ascii=False))
