f04e992c6907b033dc065c66f068f60fcdc80b6c
Ph0zzy <afayzullin@plussell.org>
2018-06-05 17:57:38 +0500

    fix dendorofecal script

--- parse.sh
+++ parse.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 OLDIFS=$IFS
 IFS=","
-FQDN = $2
-USER = $3
-while cloneaddr description
+FQDN=$2
+USER=$3
+while read cloneaddr description
  do
 	 python3 pygog.py --cloneaddr $cloneaddr --fqdn $FQDN -o $USER --description $description
  done < $1
