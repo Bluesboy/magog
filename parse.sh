4156c1e08ea1d830bcd3df3d70f31b3221e1769e
Ph0zzy <afayzullin@plussell.org>
2018-06-05 17:44:45 +0500

    dendrofecal script

---
+++ parse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OLDIFS=$IFS
+IFS=","
+FQDN = $2
+USER = $3
+while cloneaddr description
+ do
+	 python3 pygog.py --cloneaddr $cloneaddr --fqdn $FQDN -o $USER --description $description
+ done < $1
+ IFS=$OLDIFS
